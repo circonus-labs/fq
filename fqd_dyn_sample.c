d82e6869e0fa95138981cefe8983e70d737299ce
Theo Schlossnagle <theo.schlossnagle@circonus.com>
2017-09-08 07:46:51 -0400

    missed file

---
+++ fqd_dyn_sample.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2014 Circonus, Inc.
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "fqd.h"
+
+bool fqd_route_prog__sample__d(fq_msg *, int, valnode_t *);
+bool
+fqd_route_prog__sample__d(fq_msg *m, int nargs, valnode_t *args) {
+  (void)m;
+  fq_assert(nargs == 1);
+  fq_assert(args[0].value_type == RP_VALUE_DOUBLE);
+  if(drand48() < args[0].value.d) return true;
+  return false;
+}
+
